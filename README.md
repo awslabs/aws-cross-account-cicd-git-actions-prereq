566b1f70a99b20dc79a83d0f3ff329dcbf01f5ee
Damodar Shenvi Wagle <damodaw@amazon.com>
2020-04-17 10:55:47 -0400

    Initial checkin

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-## My Project
+# aws-cross-account-cicd-git-actions-prereq
 
-TODO: Fill this README out!
-
-Be sure to:
-
-* Change the title in this README
-* Edit your repository description on GitHub
+## This project is prerequisite to [aws-cross-account-cicd-git-actions](https://github.com/awslabs/aws-cross-account-cicd-git-actions). It is to be deployed via CDK (Typescript).
 
 ## License
-
 This library is licensed under the MIT-0 License. See the LICENSE file.
 
+### System requirements
+- [node (version >= 12x)](https://nodejs.org/en/download/)
+- [jq](https://github.com/stedolan/jq/wiki/Installation)
+
+### Project Structure
+> Project has been structured such that there exist one stack under root folder `src/` that deploys cross account iam role and cloudformation execution iam role to the target AWS account.
+>
+>> `src/cdk-stack.ts` deploys cross account and cloudformation execution iam roles.
+>> `src/cdk-stack-param.json` Defines parameters to be used in the stack. The only parameter currently is TOOLS_ACCOUNT_USER_ARN which defines the iam user arn that would be allowed to assume the cross account role. __In the arn replace `<tools-account-id>` with AWS account id of designated tools account.__
+>> `assume-role-policy.json` is the policy document for the user to be created in the tools account so that the user has necessary permissions to assume cross account role. __Under resources of CrossAccountAssumeRole sid replace `<target-account-id>` with AWS account id of designated target deployment account.__
+>>
+>> The stack is part of `app.ts` which represents an cdk app.
+
+### Deployment using CDK
+> The project is to be deployed as a cdk app using `deploy.sh` script provided at the root of the project folder.
+>
+>> Script takes AWS profile name configured in `~/.aws/credentials` file as the only parameter. This AWS profile should pertain to the AWS account designated as the deployment target. It also needs to have necessary permissions to create Cloudformation stack and IAM roles.
+>> Please note the Cloudformation exports `GIT-ACTIONS-CF-EXECUTION-ROLE-ARN` and `GIT-ACTIONS-CROSS-ACCOUNT-ROLE-ARN`. You will need them.
+>> ***As a prerequisite to deployment please setup a profile for each AWS account to be designated as the deployment target***.
---
+++ src/cdk-stack.ts
@@ -0,0 +1,227 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import * as core from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+
+/**
+ * Custom properties to accomodate list of code deployment buckets across different regions
+ */
+interface EnvProps extends core.StackProps{
+  toolsAccountUserArn: string;
+}
+
+export class CrossAccountRolesStack extends core.Stack {
+  constructor(scope: core.Construct, id: string, props?: EnvProps) {
+    super(scope, id, props);
+
+    // Create a boundary policy
+    // const boundaryPolicy = new iam.Policy(
+    //   this,
+    //   'BoundaryPolicy',
+    //   {
+    //     statements: [
+    //       new iam.PolicyStatement({
+    //         notActions: [
+    //           'iam:Create*',
+    //           'iam:Delete*',
+    //           'iam:Update*',
+    //           'iam:Attach*',
+    //           'iam:Detach'
+    //         ],
+    //         effect: iam.Effect.ALLOW,
+    //         resources: [
+    //           '*'
+    //         ]
+    //       })
+    //     ]
+    //   }
+    // )
+
+    // Create Cloudformation Execution Role
+    const cfExecutionRole = new iam.Role(
+      this,
+      'GitActionsCFExecutionRole',
+      {
+        assumedBy: new iam.ServicePrincipal('cloudformation.amazonaws.com'),
+        description: 'Role assumed by cloudformation service while creating the required resources',
+        roleName: 'git-action-cf-execution-role',
+        inlinePolicies: {
+          CFExecutionPolicy: new iam.PolicyDocument({
+            assignSids: true,
+            statements: [
+              new iam.PolicyStatement({
+                actions: [
+                  'iam:Get*',
+                  'iam:List*',
+                  'iam:*Role*',
+                  'iam:CreatePolicy',
+                  'iam:DeletePolicy',
+                  'iam:*PolicyVersion*',
+                  'iam:*InstanceProfile*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+                // conditions: {
+                //   'StringEquals': {
+                //     'iam:PermissionsBoundary': 'arn:aws:iam::'+this.account+':policy/'+boundaryPolicy.policyName
+                //    }
+                // }
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  's3:Get*',
+                  's3:List*',
+                  's3:HeadBucket'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  'cloudformation:*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  'apigateway:*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  'lambda:*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              })
+            ]
+          })
+        }
+      }
+    )
+
+    // Create a cross account role
+    const crossAccountRole = new iam.Role(
+      this,
+      'CrossAccountRole',
+      {
+        assumedBy: new iam.ArnPrincipal(String(props?.toolsAccountUserArn)),
+        description: 'Cross account role to be assumed by Raven tools account. Used for CICD deployments only.',
+        roleName: 'git-action-cross-account-role',
+        inlinePolicies: {
+          CrossAccountPolicy: new iam.PolicyDocument({
+            assignSids: true,
+            statements: [
+              new iam.PolicyStatement({
+                actions: [
+                  'iam:PassRole'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  cfExecutionRole.roleArn
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  's3:List*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  's3:*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  // This is staging bucket created by CDKToolkit stack when CDK app is bootstrapped
+                  'arn:aws:s3:::cdktoolkit-stagingbucket-*',
+                  'arn:aws:s3:::cdktoolkit-stagingbucket-*/*'
+                ]
+              }),
+              new iam.PolicyStatement({
+                actions: [
+                  'cloudformation:*'
+                ],
+                effect: iam.Effect.ALLOW,
+                resources: [
+                  '*'
+                ]
+              })
+            ]
+          })
+        }
+      }
+    );
+
+    // STS Session Tagging Permission
+    const sessionTaggingPolicy = new iam.PolicyStatement()
+    sessionTaggingPolicy.addPrincipals(new iam.ArnPrincipal(String(props?.toolsAccountUserArn)));
+    sessionTaggingPolicy.addActions('sts:TagSession');
+    sessionTaggingPolicy.effect = iam.Effect.ALLOW;
+    crossAccountRole.assumeRolePolicy?.addStatements(sessionTaggingPolicy)
+
+    /*********************************** List of Outputs ************************************/
+    // new core.CfnOutput(
+    //   this,
+    //   'BoundaryPolicyArn',
+    //   {
+    //     description: 'Boundary polocy for roles created through Cross Account Cloudformation Stack',
+    //     exportName: 'GIT-ACTIONS-BOUNDARY-POLICY-ARN',
+    //     value: 'arn:aws:iam::'+this.account+':policy/'+boundaryPolicy.policyName
+    //   }
+    // )
+    
+    new core.CfnOutput(
+      this,
+      'CFExecutionRoleArn',
+      {
+        description: 'Cloudformation Execution Role ARN',
+        exportName: 'GIT-ACTIONS-CF-EXECUTION-ROLE-ARN',
+        value: cfExecutionRole.roleArn
+      }
+    )
+
+    new core.CfnOutput(
+      this,
+      'CrossAccountRoleArn',
+      {
+        description: 'Cross Account Role ARN',
+        exportName: 'GIT-ACTIONS-CROSS-ACCOUNT-ROLE-ARN',
+        value: crossAccountRole.roleArn
+      }
+    )
+    /****************************************************************************************/
+  }
+}
